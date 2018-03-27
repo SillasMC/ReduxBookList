1a879d69ce9bcdd821b7cbe500f1593b72488529
Sillas Camara <sillasmc@gmail.com>
2018-03-26 21:17:16 -0300

    Show list of books

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+	return [
+		{ title : 'Js the good part' },
+		{ title : 'HP and the goblet of the Phoenix' },
+		{ title : 'The blank tower' },
+		{ title : '1964' }
+	]
+}
